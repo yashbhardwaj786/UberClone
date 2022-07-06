8f3c96615633e9012d6ab6b62eb547412f26b6a2
yashbhardwaj <yashpremsharma@gmail.com>
2022-07-06 21:07:58 +0530

    home page design done

--- src/global/data.js
+++ src/global/data.js
@@ -7,11 +7,11 @@ export const filterData = [ {name:"Ride",image: require('../../assets/ride.png')
 
 
 export const rideData =[
-    {street:"32 Olivia Rd",area:"Klipfontein 83-Ir,Boksburg",id:"0"},
-    {street:"Hughes Industrial Park",area:"Hughes,Boksburg",id:"1"},
-    {street:"32 Olivia Road",area:" East Rand,Ekurhuleni,Gauteng,1459",id:"2"},
-    {street:"Total Boksburg",area:"35 Atlas Rd,Anderbolt,Boksburg",id:"3"},
-    {street:"179 8th Ave",area:"Bezuidenhout Valley,Johannesburg",id:"4"},
+    {street:"Exam Warriors",area:"Madhav Vihar, Kotputli, Rajasthan",id:"0"},
+    {street:"Daily Khhana",area:"Madhav Vihar, Kotputli, Rajasthan",id:"1"},
+    {street:"Narayan Singh Circle",area:" Rambagh, Jaipur, Rajasthan",id:"2"},
+    {street:"Pareek & Co. Law",area:"O-16, Ashok Marg, Panch Batti, C Scheme, Jaipur, Rajasthan",id:"3"},
+    {street:"Prem Bekery",area:"34, Mangalam Vihar -D benar Road, Near Kasturi Palace, Jaipur, Rajasthan",id:"4"},
 ];
 
 export const carTypeData =[
@@ -57,9 +57,12 @@ export const paymentOptions = [{image:require('../../assets/visaIcon.png'),text:
 
 export const availableServices = ["Uber Go","UberX","Uber connect","Uber Black","Uber Van","Uber Assist"]
 
-export const carsAround = [{latitude:-26.207487,longitude:28.236226},
-    {latitude:-26.202616,longitude:28.227718},
-    {latitude:-26.202424,longitude:28.236612},
-    {latitude:-26.208565,longitude:28.237191},
-    {latitude:-26.203598,longitude:28.239509},
+export const carsAround = [
+    {latitude:27.69755,longitude:76.19599},
+    {latitude:27.698569,longitude:76.196996},
+    {latitude:27.696459,longitude:76.195308},
+    {latitude:27.697382,longitude:76.195989},
+    {latitude:27.697413,longitude:76.194812},
+    {latitude:27.697981,longitude:76.195142},
+    {latitude:27.698887,longitude:76.196480},
 ]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,16 +7,67 @@ import {
   ScrollView,
   Image,
   FlatList,
+  Permission,
+  PermissionsAndroid,
+  Platform,
+  Alert,
 } from 'react-native';
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {colors, parameters} from '../global/styles';
 import {Icon} from 'react-native-elements';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {filterData} from '../global/data';
+import {filterData, carsAround} from '../global/data';
+import MapView, {Marker} from 'react-native-maps';
+import {mapStyle} from '../global/mapStyle';
+import Geolocation from '@react-native-community/geolocation';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const HomeScreen = () => {
+  const [latlong, setLatLong] = useState({});
+  const _map = useRef(1);
+
+  useEffect(() => {
+    checkPermission();
+      // console.log(latlng)
+      [];
+  });
+
+  const checkPermission = async () => {
+    if (Platform.OS == 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getLocation();
+        } else {
+         askPermission();
+        }
+      } catch (err) {
+        console.log('catch');
+      }
+    }
+  };
+
+  const askPermission = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,);
+      PermissionsAndroid.RESULTS.GRANTED = granted;
+      getLocation();
+  };
+
+  const getLocation = async () => {
+    try {
+      Geolocation.getCurrentPosition(data => {
+        setLatLong({lattitude: data.coords.latitude, longitude: data.coords.longitude});
+
+      })
+    }catch(err){
+        console.warn("Error")
+    }
+  };
+
   return (
     <View styles={styles.container}>
       <View style={styles.header}>
@@ -50,11 +101,11 @@ const HomeScreen = () => {
               />
             </View>
           </View>
-          <View>
-          <ScrollView horizontal={true} style={{ width: "100%" }}>
+        </View>
+        <View>
+          <ScrollView horizontal={true} style={{width: '100%'}}>
             <FlatList
               numColumns={4}
-              
               showHorizontalScrollIndicator={false}
               data={filterData}
               keyExtractor={item => item.id}
@@ -65,13 +116,128 @@ const HomeScreen = () => {
                   </View>
                   <View>
                     <Text style={styles.title}>{item.name}</Text>
-                    </View>
+                  </View>
                 </View>
               )}
             />
-            </ScrollView>
+          </ScrollView>
+        </View>
+
+        <View style={styles.view3}>
+          <Text style={styles.text3}> Where to? </Text>
+
+          <View style={styles.view4}>
+            <Icon
+              type="material-community"
+              name="clock-time-four"
+              color={colors.grey1}
+              size={26}
+            />
+            <Text style={{marginLeft: 5}}> Now</Text>
+
+            <Icon
+              type="material-community"
+              name="chevron-down"
+              color={colors.grey1}
+              size={26}
+            />
           </View>
         </View>
+
+        <View style={styles.view5}>
+          <View style={styles.view6}>
+            <View style={styles.view7}>
+              <Icon
+                type="material-community"
+                name="map-marker"
+                color={colors.blacck}
+                size={22}
+              />
+            </View>
+            <View>
+              <Text style={{fontSize: 18, color: colors.black}}>
+                {' '}
+                Exam Warriors{' '}
+              </Text>
+              <Text style={{color: colors.grey3}}>
+                {' '}
+                Madhav Vihar, Kotputli, Rajasthan{' '}
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Icon
+              type="material-community"
+              name="chevron-right"
+              vron-right
+              color={colors.grey}
+              size={26}
+            />
+          </View>
+        </View>
+
+        <View style={styles.view5copy}>
+          <View style={styles.view6}>
+            <View style={styles.view7}>
+              <Icon
+                type="material-community"
+                name="map-marker"
+                color={colors.blacck}
+                size={22}
+              />
+            </View>
+            <View>
+              <Text style={{fontSize: 18, color: colors.black}}>
+                {' '}
+                Narayan Singh Circle{' '}
+              </Text>
+              <Text style={{color: colors.grey3, flex: 1}}>
+                {' '}
+                Rambagh, Jaipur, Rajasthan{' '}
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Icon
+              type="material-community"
+              name="chevron-right"
+              vron-right
+              color={colors.grey}
+              size={26}
+            />
+          </View>
+        </View>
+        <Text style={styles.text4}>Around you!</Text>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: 80,
+          }}>
+          <MapView
+            ref={_map}
+            provider={MapView.PROVIDER_GOOGLE}
+            style={styles.map}
+            customMapStyle={mapStyle}
+            showsUserLocation={true}
+            followsUserLocation={true}
+            initialRegion={{
+              ...carsAround[0],
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }}>
+            {carsAround.map((item, index) => (
+              <MapView.Marker coordinate={item} key={index.toString()}>
+                <Image
+                  source={require('../../assets/carMarker.png')}
+                  style={styles.carsAround}
+                  resizeMode="cover"
+                />
+              </MapView.Marker>
+            ))}
+          </MapView>
+        </View>
+        <View style={styles.emptyView}></View>
       </ScrollView>
       <StatusBar style="light" backgroundColor="#2058c0" />
     </View>
@@ -182,6 +348,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
+  view5copy: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 25,
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+    flex: 1,
+  },
+
   view6: {
     alignItems: 'center',
     flex: 5,
@@ -198,7 +374,7 @@ const styles = StyleSheet.create({
   },
 
   map: {
-    height: 150,
+    height: 200,
     marginVertical: 0,
     width: SCREEN_WIDTH * 0.92,
   },
@@ -223,4 +399,5 @@ const styles = StyleSheet.create({
   },
 
   view9: {width: 4, height: 4, borderRadius: 2, backgroundColor: 'white'},
+  emptyView: {width: SCREEN_WIDTH, height: 100},
 });
